0efd0c016a2e5416f14622c55b11b08ccdd6e8de
Asif <saluasif@gmail.com>
2016-07-08 21:05:23 +0300

    updated gitignore to remove .js

--- src/inventory/shared/common.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function getObjectValue(instance, field) {
-    if (!instance) {
-        return '';
-    }
-    return instance[field];
-}
-exports.getObjectValue = getObjectValue;
-//# sourceMappingURL=common.js.map
--- src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_component_1 = require('./inventory/app.component');
-var app_routes_1 = require('./inventory/app.routes');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
-    app_routes_1.APP_ROUTER_PROVIDERS
-]);
-//# sourceMappingURL=main.js.map
